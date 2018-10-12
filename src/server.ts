e44e08f384c25e0a154a5181ea9cfa2106007300
Josh Ghent <me@joshghent.com>
2018-10-12 15:40:56 +0100

    Updated server to remove authentication

--- src/server.ts
+++ src/server.ts
@@ -17,31 +17,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(logger("common"));
 
-const PASSWORD = process.env.PASSWORD || "password123";
-const RUNTIME_SECRET = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-
 app.use(express.static(path.resolve(__dirname, "../static")));
 
-app.get("/" + RUNTIME_SECRET, (req, res) => {
+app.get("/", (req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(getScoreboardPage(reportCache.get()));
 });
 
-app.get("/test/:secret", (req, res) => {
-    res.json({ matched: req.params.secret === RUNTIME_SECRET });
-});
-
-app.post("/authenticate", (req, res) => {
-    if(req.body.password === PASSWORD) {
-        res.redirect("/" + RUNTIME_SECRET);
-    }
-});
-
-
-app.get("/*", (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(getUnauthorisedPage());
-});
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     if (!(err instanceof HttpErrors.HttpError)) {
